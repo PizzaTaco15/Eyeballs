573af6370fcfa8e6f20b87014aa5ebf696972945
PizzaTaco15 <88344372+PizzaTaco15@users.noreply.github.com>
2021-08-13 16:53:59 -0600

    Add files via upload

---
+++ eyes.js
@@ -0,0 +1,19 @@
+const balls = document.getElementsByClassName('ball');
+const balls2 = document.getElementsByClassName('ball2');
+for (var i = 0; i < 2; i++) {
+  document.onmousemove = (event) => {
+  const x = (event.clientX * 100) / window.innerWidth + '%';
+  const y = (event.clientY * 100) / window.innerHeight + '%';
+  console.log(x)
+
+  
+  balls[0].style.left = x;
+  balls[0].style.top = y;
+  balls[0].transform = 'translate(-' + x + ',-' + y + ')';
+  balls2[0].style.left = x;
+  balls2[0].style.top = y;
+  balls2[0].transform = 'translate(-' + x + ',-' + y + ')';
+  
+  
+};
+};
